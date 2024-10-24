f4859ea1777222cda250673d718b1a6253120ee2
Xieshen Zhang <xiezhang@redhat.com>
2024-10-24 17:11:56 -0400

    Create controller for OdhNIMAccount

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/kserve/kserve v0.14.0
 	github.com/onsi/ginkgo/v2 v2.20.1
 	github.com/onsi/gomega v1.34.1
+	github.com/opendatahub-io/opendatahub-operator/v2 v2.19.0
 	github.com/openshift/api v0.0.0-20230823114715-5fdd7511b790
 	github.com/spf13/cobra v1.8.1
 	k8s.io/api v0.31.0
@@ -28,9 +29,11 @@ require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/emicklei/go-restful/v3 v3.12.1 // indirect
+	github.com/evanphx/json-patch v5.6.0+incompatible // indirect
 	github.com/evanphx/json-patch/v5 v5.9.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-errors/errors v1.4.2 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-logr/zapr v1.3.0 // indirect
@@ -42,15 +45,19 @@ require (
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/gnostic v0.6.9 // indirect
+	github.com/google/gnostic-models v0.6.8 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/go-containerregistry v0.20.2 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/pprof v0.0.0-20240827171923-fa2c70bbbfe5 // indirect
 	github.com/google/s2a-go v0.1.8 // indirect
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.3 // indirect
 	github.com/googleapis/gax-go/v2 v2.13.0 // indirect
 	github.com/googleapis/google-cloud-go-testing v0.0.0-20210719221736-1c9a4c676720 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/imdario/mergo v0.3.16 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.1-0.20220621161143-b0104c826a24 // indirect
@@ -61,21 +68,27 @@ require (
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/monochromegane/go-gitignore v0.0.0-20200626010858-205db1a8cc00 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/openshift/custom-resource-status v1.1.2 // indirect
+	github.com/operator-framework/api v0.18.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_golang v1.20.2 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.57.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/rogpeppe/go-internal v1.13.1 // indirect
+	github.com/sirupsen/logrus v1.9.2 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/xlab/treeprint v1.2.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.54.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.54.0 // indirect
 	go.opentelemetry.io/otel v1.29.0 // indirect
 	go.opentelemetry.io/otel/metric v1.29.0 // indirect
 	go.opentelemetry.io/otel/trace v1.29.0 // indirect
+	go.starlark.net v0.0.0-20200306205701-8dd3e2ee1dd5 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/crypto v0.26.0 // indirect
@@ -99,7 +112,7 @@ require (
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/apiextensions-apiserver v0.30.4 // indirect
-	k8s.io/client-go v0.30.4 // indirect
+	k8s.io/client-go v11.0.0+incompatible // indirect
 	k8s.io/klog/v2 v2.130.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20240827152857-f7e401e7b4c2 // indirect
 	k8s.io/utils v0.0.0-20240821151609-f90d01438635 // indirect
@@ -107,6 +120,8 @@ require (
 	knative.dev/pkg v0.0.0-20240815051656-89743d9bbf7c // indirect
 	knative.dev/serving v0.42.2 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
+	sigs.k8s.io/kustomize/api v0.13.4 // indirect
+	sigs.k8s.io/kustomize/kyaml v0.16.0 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
 	sigs.k8s.io/yaml v1.4.0 // indirect
 )
--- pkg/controllers/odhnimaccount_controller.go
+++ pkg/controllers/odhnimaccount_controller.go
@@ -6,10 +6,17 @@ import (
 	"context"
 
 	"github.com/opendatahub-io/odh-nim-operator/api/v1alpha1"
+	dscv1 "github.com/opendatahub-io/opendatahub-operator/v2/apis/datasciencecluster/v1"
+	tmpv1 "github.com/openshift/api/template/v1"
 
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	apimeta "k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 // +kubebuilder:rbac:groups=nim.opendatahub.io,resources=odhnimaccounts,verbs=get;list;watch;create;update;patch;delete
@@ -24,6 +31,16 @@ type OdhNIMAccountController struct {
 	Scheme *runtime.Scheme
 }
 
+const (
+	kServeCondition    = "KServeReadiness"
+	apiKeyCondition    = "APIKeyValidation"
+	templateCondition  = "TemplateUpdate"
+	configMapCondition = "ConfigMapUpdate"
+
+	kServeNotReady = "KServeNotReady"
+	kServeReady    = "KServeReady"
+)
+
 // SetupWithManager is used for setting up the controller with a manager (check the init function)
 func (r *OdhNIMAccountController) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
@@ -33,5 +50,198 @@ func (r *OdhNIMAccountController) SetupWithManager(mgr ctrl.Manager) error {
 }
 
 func (r *OdhNIMAccountController) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	logger := log.FromContext(ctx).
+		WithName("odh-nim-account-controller").
+		WithValues("OdhNIMAccount", req.NamespacedName)
+
+	ctx = log.IntoContext(ctx, logger)
+
+	logger.V(1).Info("got request for OdhNIMAccount")
+
+	var account v1alpha1.OdhNIMAccount
+	if err := r.Get(ctx, req.NamespacedName, &account); err != nil {
+		if errors.IsNotFound(err) {
+			logger.V(1).Info("OdhNIMAccount not found")
+			return ctrl.Result{}, nil
+		}
+		logger.Error(err, "error fetching OdhNIMAccount")
+		return ctrl.Result{}, err
+	}
+
+	if ready, err := r.checkKServeReady(ctx); err != nil {
+		conditions := []metav1.Condition{
+			{
+				Type:   kServeCondition,
+				Status: metav1.ConditionFalse,
+				Reason: kServeNotReady,
+			},
+		}
+		_, _ = r.updateStatus(ctx, &account, conditions, account.Status.TemplateRef, account.Status.ConfigMapRef)
+
+		logger.Error(err, "error fetching DataScienceCluster")
+		return ctrl.Result{}, err
+	} else if !ready {
+		if r, e := r.handleKServeNotReady(ctx, &account); e != nil {
+			logger.Error(e, "error updating OdhNIMAccount status")
+			return ctrl.Result{}, err
+		} else if r {
+			logger.V(1).Info("OdhNIMAccount modified")
+			return ctrl.Result{Requeue: true}, nil
+		}
+		logger.V(1).Info("KServe not ready")
+		return ctrl.Result{Requeue: true}, nil
+	}
+
+	// TODO write code
+	// 1. Fetch OdhNimApp
+	// 2. If OdhNimApp NOT found (deleted):
+	//		- Break reconciliation, we use the finalizer mechanism to cleanups
+	//
+	// 3. If in deletion process !OdhNimApp.DeletionTimestamp.IsZero() (note the !):
+	//		3.1 If has our finalizer "nim.opendatahub.io/cleanup_finalizer" (const in controllers.go):
+	//			- Tear down (if we want)
+	//			- Remove our finalizer
+	//		3.2 Break reconciliation
+	//
+	// 4. If doesn't have our finalizer "nim.opendatahub.io/cleanup_finalizer":
+	//		- Add the finalizer
+	//
+	// 5. If OdhNimApp.Spec.ApiKey.Validate is True (defaults to true):
+	//		5.1 Validate the API Key!!
+	// 		5.2 Patch OdhNimApp.Status.Condition[Type=ApiKeyValidated] to True/False based on the validation (consts in controllers.go)
+	//		5.3 Patch OdhNimApp.Spec.ApiKey.Validate to False (but store the original value for step 7)
+	//		5.4 If the validation NOT successful:
+	//			- Do we want to tear down?
+	//			- Break reconciliation
+	//
+	// 6. Reconcile the Template, OdhNimApp.Spec.TemplateRef, if empty, create and patch the reference
+	//
+	// 7. If OdhNimApp.Spec.ApiKey.Validate WAS True (before 5.3) OR
+	//	OdhNimApp.Spec.Content.Update IS True OR
+	//	OdhNimApp.Spec.Content.ConfigMapRef is empty (we haven't created it yet):
+	//		7.1 Fetch NIM Images and models
+	//		7.2 Patch OdhNimApp.Status.Condition[Type=ContentUpdated] to True/False based on the fetch status (consts in controllers.go)
+	//		7.3 Patch OdhNimApp.Spec.Content.Update to False (if wasn't false to begin with)
+	//		7.4 If the fetching was successful:
+	//			- Reconcile the content ConfigMap with the updated data
+	//			- Patch the OdhNimApp.Spec.Content.ConfigMapRef with the reference to the ConfigMap if empty
+	//
+	// 8.  Reconcile a daily recurring Cron Job owned by this OdhNimApp, patching OdhNimApp.Spec.Content.Update to True
+
 	return ctrl.Result{}, nil
+}
+
+func (r *OdhNIMAccountController) updateStatus(ctx context.Context, account *v1alpha1.OdhNIMAccount,
+	conditions []metav1.Condition, template *corev1.ObjectReference, configMap *corev1.ObjectReference) (bool, error) {
+	for _, condition := range conditions {
+		apimeta.SetStatusCondition(&account.Status.Conditions, condition)
+	}
+
+	account.Status.TemplateRef = template
+	account.Status.ConfigMapRef = configMap
+
+	if err := r.Status().Update(ctx, account); err != nil {
+		if errors.IsConflict(err) {
+			return true, nil
+		} else {
+			return false, err
+		}
+	}
+	return false, nil
+}
+
+func (r *OdhNIMAccountController) checkKServeReady(ctx context.Context) (bool, error) {
+	var dscList dscv1.DataScienceClusterList
+	if err := r.List(ctx, &dscList); err != nil {
+		return false, err
+	}
+
+	if len(dscList.Items) > 0 {
+		dsc := dscList.Items[0]
+		if dsc.Status.Phase == "Ready" {
+			ready, ok := dsc.Status.InstalledComponents["kserve"]
+			if ok && ready {
+				return true, nil
+			}
+		}
+	}
+
+	return false, nil
+}
+
+func (r *OdhNIMAccountController) handleKServeNotReady(ctx context.Context, account *v1alpha1.OdhNIMAccount) (bool, error) {
+	conditions := []metav1.Condition{
+		{
+			Type:   kServeCondition,
+			Status: metav1.ConditionFalse,
+			Reason: kServeNotReady,
+		},
+		{
+			Type:   apiKeyCondition,
+			Status: metav1.ConditionUnknown,
+			Reason: kServeNotReady,
+		},
+		{
+			Type:   templateCondition,
+			Status: metav1.ConditionFalse,
+			Reason: kServeNotReady,
+		},
+		{
+			Type:   configMapCondition,
+			Status: metav1.ConditionFalse,
+			Reason: kServeNotReady,
+		},
+	}
+
+	rq, err := r.updateStatus(ctx, account, conditions, nil, nil)
+
+	if err == nil {
+		err = r.deleteTemplate(ctx, account)
+	}
+
+	if err == nil {
+		err = r.deleteConfigMap(ctx, account)
+	}
+
+	return rq, err
+}
+
+func (r *OdhNIMAccountController) deleteTemplate(ctx context.Context, account *v1alpha1.OdhNIMAccount) error {
+	if account.Status.TemplateRef != nil {
+		var template tmpv1.Template
+		if err := r.Get(ctx,
+			client.ObjectKey{
+				Namespace: account.Status.TemplateRef.Namespace,
+				Name:      account.Status.TemplateRef.Name},
+			&template); err != nil {
+			if !errors.IsNotFound(err) {
+				return err
+			}
+		} else {
+			if e := r.Delete(ctx, &template); e != nil {
+				return e
+			}
+		}
+	}
+	return nil
+}
+
+func (r *OdhNIMAccountController) deleteConfigMap(ctx context.Context, account *v1alpha1.OdhNIMAccount) error {
+	if account.Status.ConfigMapRef != nil {
+		var cm corev1.ConfigMap
+		if err := r.Get(ctx,
+			client.ObjectKey{
+				Namespace: account.Status.ConfigMapRef.Namespace,
+				Name:      account.Status.ConfigMapRef.Name},
+			&cm); err != nil {
+			if !errors.IsNotFound(err) {
+				return err
+			}
+		} else {
+			if e := r.Delete(ctx, &cm); e != nil {
+				return e
+			}
+		}
+	}
+	return nil
 }
